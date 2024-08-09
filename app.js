0e5f0a529334652f352422ab6c98d66827a895bc
stella <ramyastella08@gmail.com>
2024-08-09 22:28:30 +0530

    port changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:8100'
 }));
 
 // MongoDB connection
