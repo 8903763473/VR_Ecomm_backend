c0a9e996cd8048b682e5fef2235db982d0b8d1ba
stella <ramyastella08@gmail.com>
2024-08-23 22:08:43 +0530

    Commit by stella

--- routes/cartRouters.js
+++ routes/cartRouters.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const CartController = require('../controllers/cartController');
 
-router.get('/getmyCart', CartController.getCart);
+router.post('/getmyCart', CartController.getCart);
 router.post('/addtoCart', CartController.addToCart);
 router.delete('/remove/:productId', CartController.removeFromCart);
 router.delete('/clearMyCart/:userId', CartController.clearCart);
